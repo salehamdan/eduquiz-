79405af527d670c825a9d1475d0e042072f734c6
salehamdan <salehamdan@hotmail.com>
2026-06-21 21:24:07 +0300

    restore stable index7 and kill service worker cache

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,12 @@
-self.addEventListener('install', function(e){ self.skipWaiting(); });
-self.addEventListener('activate', function(e){
-  e.waitUntil(
-    caches.keys().then(function(keys){
-      return Promise.all(keys.map(function(k){ return caches.delete(k); }));
-    }).then(function(){
-      return self.registration.unregister();
-    }).then(function(){
-      return self.clients.matchAll();
-    }).then(function(clients){
-      clients.forEach(function(c){ c.navigate(c.url); });
-    })
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.map(key => caches.delete(key))))
+      .then(() => self.registration.unregister())
+      .then(() => self.clients.matchAll({ type: 'window' }))
+      .then(clients => clients.forEach(client => client.navigate(client.url)))
   );
 });
-self.addEventListener('fetch', function(e){});
